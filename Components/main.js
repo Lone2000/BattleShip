1919895e1e23b30dddf35f076dfc2682d144f9ee
Lone2000 <Lone0059@gmail.com>
2024-06-14 15:12:25 +0500

    Update:Basic Ships Flipping Feature Logic Implemented

--- Components/main.js
+++ Components/main.js
@@ -1,3 +1,16 @@
-import board from './board'
+const optionContainer = document.querySelector('.option-container');
+const flipButton = document.querySelector('#flip-button');
 
-console.log(board);
+// Option Choosing
+let angle = 0;
+function flip(){
+    const optionShips = Array.from((optionContainer.children));
+    angle = angle === 0 ? 90 : 0;
+
+    optionShips.forEach(ship =>{
+        ship.style.transform = `rotate(${angle}deg)`;
+    })
+}
+
+
+flipButton.addEventListener('click', flip)
