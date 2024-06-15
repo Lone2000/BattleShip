6c104e5237d4a91c0c4f7183c7e076179cf1725e
Lone2000 <Lone0059@gmail.com>
2024-06-15 12:08:39 +0500

    Update: AddShipPiece Computer logic

--- Components/main.js
+++ Components/main.js
@@ -2,6 +2,8 @@ const gamesBoardContainer = document.querySelector('#gamesboard-container')
 const optionContainer = document.querySelector('.option-container');
 const flipButton = document.querySelector('#flip-button');
 const startButton = document.querySelector('#start-button');
+
+
 // Option Choosing
 let angle = 0;
 function flip(){
@@ -12,7 +14,7 @@ function flip(){
         ship.style.transform = `rotate(${angle}deg)`;
     })
 }
-
+flipButton.addEventListener('click', flip)
 
 // Creating Boards
 const width = 10;
@@ -34,7 +36,52 @@ function createBoard(color, user){
     gamesBoardContainer.append(gameBoardContainer);
 }
 
-flipButton.addEventListener('click', flip)
-
 createBoard('brown', 'player');
 createBoard('orange', 'computer');
+
+
+// Creating Ships
+
+class Ship{
+    constructor(name, length){
+        this.name = name;
+        this.length = length;
+    }
+}
+
+const destroyer = new Ship('destroyer', 2);
+const submarine = new Ship('submarine', 3);
+const cruiser = new Ship('cruiser', 3);
+const battleship = new Ship('battleship', 4);
+const carrier = new Ship('carrier', 5);
+
+const ships =[destroyer, submarine, cruiser, battleship, carrier];
+
+function addShipPiece(ship){
+    // Pick out All board blocks
+    const allBoardBlocks = document.querySelectorAll('#computer div');
+    let randomBoolean = Math.random() < 0.5
+    let isHorizontal = randomBoolean;
+    let randomStartIndex = Math.floor(Math.random() * (width * width)) // By Default random gives 0 ~ 1
+    console.log(randomStartIndex);
+
+
+    let shipBlocks = [];
+    for(let i = 0; i < ship.length; i++){
+        if(isHorizontal){
+            shipBlocks.push(allBoardBlocks[Number(randomStartIndex) + i]);
+        }else{
+            shipBlocks.push(allBoardBlocks[Number(randomStartIndex) + (i * width)]);
+        }
+    }
+
+    console.log(shipBlocks);
+
+}
+
+
+addShipPiece(carrier);
+
+
+
+
